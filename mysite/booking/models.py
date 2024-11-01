5e9913f8f27ff2b9d1af869d6e513d28861f3da1
PabloGMD <pablogmdsoftware@gmail.com>
2024-11-01 18:46:02 +0100

    Create bus model

--- mysite/booking/models.py
+++ mysite/booking/models.py
@@ -13,3 +13,12 @@ class Customer(models.Model):
     def __str__(self):
         name = f"{self.first_name} {self.last_name}".title()
         return name
+
+class Bus(models.Model):
+    """Table to store bus entities owned by the business, the id in this country will 
+    be XXYY where X are letters and Y are numbers. Sears number does not have into
+    account the other types of seats."""
+    bus_id = models.CharField(max_length=7)
+    seats = models.PositiveSmallIntegerField()
+    seats_first_row = models.PositiveSmallIntegerField()
+    seats_reduced_mobility = models.PositiveSmallIntegerField()
