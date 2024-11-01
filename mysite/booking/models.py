e4c0995fabbb967ce7b8c52c50baed28d73c47b0
PabloGMD <pablogmdsoftware@gmail.com>
2024-11-01 18:27:14 +0100

    Create customers model

--- mysite/booking/models.py
+++ mysite/booking/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    """Customers and their necessary information to buy tickets"""
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=50)
+    birt_date = models.DateField()
+    gmail = models.EmailField()
+    registration_datetime = models.DateTimeField(auto_now_add=True)
+    has_large_family = models.BooleanField(default=False)
+    has_reduced_mobility = models.BooleanField(default=False)
+
+    def __str__(self):
+        name = f"{self.first_name} {self.last_name}".title()
+        return name
