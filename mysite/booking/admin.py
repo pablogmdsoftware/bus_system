96ea8c19558691e1df26552a5103c9ef40a1c614
PabloGMD <pablogmdsoftware@gmail.com>
2024-11-02 12:17:09 +0100

    Register pass model

--- mysite/booking/admin.py
+++ mysite/booking/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Customer, Bus, PassType
+from .models import Customer, Bus, PassType, Pass
 
 admin.site.register(Customer)
 admin.site.register(Bus)
 admin.site.register(PassType)
+admin.site.register(Pass)
