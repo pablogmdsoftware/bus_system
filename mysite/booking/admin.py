aaa8edeb1d5b0a8d8f70f595d17635dffbec6ac4
PabloGMD <pablogmdsoftware@gmail.com>
2024-11-04 10:52:09 +0100

    Register travel model

--- mysite/booking/admin.py
+++ mysite/booking/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Customer, Bus, PassType, Pass
+from .models import Customer, Bus, PassType, Pass, Travel
 
 admin.site.register(Customer)
 admin.site.register(Bus)
 admin.site.register(PassType)
 admin.site.register(Pass)
+admin.site.register(Travel)
