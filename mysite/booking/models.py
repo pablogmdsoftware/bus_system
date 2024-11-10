ddfa41e2e69e0033fa0192cd2adcc6b7fae4cb1b
PabloGMD <pablogmdsoftware@gmail.com>
2024-11-10 12:20:39 +0100

    Fix birt_date typo in customer model

--- mysite/booking/models.py
+++ mysite/booking/models.py
@@ -7,7 +7,7 @@ class Customer(models.Model):
     """Customers and their necessary information to buy tickets."""
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=50)
-    birt_date = models.DateField()
+    birth_date = models.DateField()
     gmail = models.EmailField()
     registration_datetime = models.DateTimeField(auto_now_add=True)
     has_large_family = models.BooleanField(default=False)
