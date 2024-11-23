bf7c68e21691d8cec2cac57cbfbfa5d782b53459
PabloGMD <pablogmdsoftware@gmail.com>
2024-11-23 20:51:22 +0100

    Improve readability and performance of select_ticket view

--- mysite/booking/views.py
+++ mysite/booking/views.py
@@ -12,22 +12,21 @@ def travel(request):
 
 def select_ticket(request):
     context = {"cities": CITIES,"request":request.POST}
-    if request.POST["action"] == "Search":
-        form = SearchTravelForm(request.POST)
-        if form.is_valid():
-            date = form.cleaned_data["date"]
-            origin = form.cleaned_data["origin"]
-            destination = form.cleaned_data["destination"]
-            travels = Travel.objects.filter(schedule__date=date).filter(
-                origin=origin,
-                destination=destination,
-            )
-            travel_times = [travel.schedule.strftime("%H:%M") for travel in travels]
-            context.update({"travel_times":travel_times})
-        else:
-            context.update({"dict":form.errors.as_data()})
+    form = SearchTravelForm(request.POST)
+    if form.is_valid():
+        date = form.cleaned_data["date"]
+        origin = form.cleaned_data["origin"]
+        destination = form.cleaned_data["destination"]
+        travels = Travel.objects.filter(schedule__date=date).filter(
+            origin=origin,
+            destination=destination,
+        )
+        travel_times = [travel.schedule.strftime("%H:%M") for travel in travels]
+        context.update({"travel_times":travel_times})
+    else:
+        context.update({"dict":form.errors.as_data()})
             
-    elif request.POST["action"] == "Buy":
+    if request.POST["action"] == "Buy":
         form2 = PurchaseTicketForm(request.POST)
         if form2.is_valid():
             time = form2.cleaned_data["time"]
@@ -51,22 +50,8 @@ def select_ticket(request):
                 context.update({"dict":"The seat you chose is already sold, please select another"})
             else:
                 return HttpResponseRedirect(reverse("booking:confirm"))
-
         else:
-            form = SearchTravelForm(request.POST)
-            if form.is_valid():
-                date = form.cleaned_data["date"]
-                origin = form.cleaned_data["origin"]
-                destination = form.cleaned_data["destination"]
-                travels = Travel.objects.filter(schedule__date=date).filter(
-                    origin=origin,
-                    destination=destination,
-                )
-                travel_times = [travel.schedule.strftime("%H:%M") for travel in travels]
-                context.update({"travel_times":travel_times})
-                context.update({"dict":form2.errors.as_data()})
-            else:
-                context.update({"dict":form.errors.as_data()})
+            context.update({"dict2":form2.errors.as_data()})
 
     return render(request,"booking/select_ticket.html",context)
 
