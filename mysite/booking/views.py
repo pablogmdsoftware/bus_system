48443ebf6434f2102c102585748ea07d06dfef09
PabloGMD <pablogmdsoftware@gmail.com>
2024-11-23 18:10:29 +0100

    Improve form to keep travel times in it after a failed request

--- mysite/booking/views.py
+++ mysite/booking/views.py
@@ -24,13 +24,27 @@ def select_ticket(request):
             context.update({"travel_times":travel_times})
         else:
             context.update({"dict":form.errors.as_data()})
+            
     elif request.POST["action"] == "Buy":
-        form = PurchaseTicketForm(request.POST)
-        if form.is_valid():
+        form2 = PurchaseTicketForm(request.POST)
+        if form2.is_valid():
             pass
-        else:            
-            context.update({"dict":form.errors.as_data()})
-        
+        else:
+            form = SearchTravelForm(request.POST)
+            if form.is_valid():
+                date = form.cleaned_data["date"]
+                origin = form.cleaned_data["origin"]
+                destination = form.cleaned_data["destination"]
+                travels = Travel.objects.filter(schedule__date=date).filter(
+                    origin=origin,
+                    destination=destination,
+                )
+                travel_times = [travel.schedule.strftime("%H:%M") for travel in travels]
+                context.update({"travel_times":travel_times})
+                context.update({"dict":form2.errors.as_data()})
+            else:
+                context.update({"dict":form.errors.as_data()})
+
     return render(request,"booking/select_ticket.html",context)
 
 def confirm_ticket(request):
