d6fe089d29b7dad0b2c91afc3101eafd3fbb2e03
PabloGMD <pablogmdsoftware@gmail.com>
2024-11-23 17:55:08 +0100

    Add search travels functionality

--- mysite/booking/views.py
+++ mysite/booking/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import SearchTravelForm, PurchaseTicketForm
-from .models import CITIES
+from .models import Travel, CITIES
 
 def travel(request):
     context = {
@@ -13,7 +13,15 @@ def select_ticket(request):
     if request.POST["action"] == "Search":
         form = SearchTravelForm(request.POST)
         if form.is_valid():
-            pass
+            date = form.cleaned_data["date"]
+            origin = form.cleaned_data["origin"]
+            destination = form.cleaned_data["destination"]
+            travels = Travel.objects.filter(schedule__date=date).filter(
+                origin=origin,
+                destination=destination,
+            )
+            travel_times = [travel.schedule.strftime("%H:%M") for travel in travels]
+            context.update({"travel_times":travel_times})
         else:
             context.update({"dict":form.errors.as_data()})
     elif request.POST["action"] == "Buy":
